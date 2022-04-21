c675a2fa5621ec1b6b009791536dc1c642d104b8
evinaycan <aycankapazan@gmail.com>
2022-04-21 11:51:54 +0300

    doing final work on hello

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Hello world");
+        System.out.println("Batch EU8 learning git and github");
 
 
     }
